e8ec72a279be9aaac5e2590763d136f42572ceb8
jamebozo <jamebozo@gmail.com>
2017-11-06 15:04:39 +0800

    This is first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+int main ()
+{
+   //ListNodeTest()    ;
+   // MultiplyStringEntry();
+   // M7_Reverse_Integer();
+   // M3_LongestSubString();
+   //M5_LongestPalindromicSubstring();
+  printf ("Sum of x+y = %i", 1);
+}
